fb5819d91fe833cc348e38d19231a712c60b25bc
Gjergj Kadriu <gjergjk71@gmail.com>
2023-07-22 20:13:52 +0200

    Update settings.py

--- PimlurMOOC/settings.py
+++ PimlurMOOC/settings.py
@@ -66,7 +66,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gk210076k@riinvest.net'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD =  ""
 DEFAULT_FROM_EMAIL = "Helpdesk <helpdesk@riinvest>"
 
